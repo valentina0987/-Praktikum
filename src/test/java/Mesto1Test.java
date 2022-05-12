5e41c6364fdd64dbd2b24933265e6b0754eede3b
valentinazamotaeva <valentina.zamotaeva@yandex.ru>
2022-05-12 19:03:02 +0400

    new version3

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -54,7 +54,7 @@ public class Mesto1Test {
         given()
                 .auth().oauth2(bearerToken) // Передаём токен для аутентификации
                 .put("/api/cards/{photoId}/likes", photoId) // Делаем PUT-запрос
-                .then().assertThat().statusCode(200); // Проверяем, что сервер вернул код 200
+                .then().assertThat().statusCode(400); // Проверяем, что сервер вернул код 200
     }
 
     @Step("Delete like from the photo by id")
